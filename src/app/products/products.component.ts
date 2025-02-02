5ffc90984ac10a32e677322308fee96e0220db87
Rutvik bhatt <rutikbhatt83477@gmail.com>
2025-02-03 01:12:02 +0530

    Developed Category wise product images with details page dialog.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,31 +9,116 @@ export class ProductsComponent {
 
   isMobileCategoriesVisible = false;
   productCategories: any = [
-    { isExpanded: false, name: 'Continues glass railling' },
-    { isExpanded: false, name: 'Side mounted glass railling' },
-    { isExpanded: false, name: 'Staircase glass railling' },
-    { isExpanded: false, name: 'Bracket glass railling' },
-    { isExpanded: false, name: 'Handrail & Accessories' },
+    { isExpanded: false, name: 'BRACKET & COVER SYSTEM' },
+    { isExpanded: false, name: 'CONTINUOUS SYSTEM' },
+    { isExpanded: false, name: 'BRACKET SYSTEM' },
+    { isExpanded: false, name: 'HANDRAIL & ACCESSORIES' }
   ]
-  handrailAndAccessoriesImages: any = [
-    { image: 'assets/items/1.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/2.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/3.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/4.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/5.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/6.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/7.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/8.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/9.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/10.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/11.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/12.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/13.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/14.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
-    { image: 'assets/items/15.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+
+  bracketAndCoverSystemImages: any = [
+    { image: 'assets/items/1.png',
+      subImages: [
+        {title:'End Cap',image:'assets/items/1.png'},
+        {title:'Slotted End Cap',image:'assets/items/1.png'}
+      ],
+      category: 'BRACKET & COVER SYSTEM',
+      ProductName: 'Square Bracket & cover', 
+      essentialDetails: {
+        type: 'Bracket and Cover System',
+        use: 'Indoor and Outdoor',
+        material: 'Aluminium Alloy 6063-T6',
+        length: ['Square Bracket (65mm)', 'Square Bracket (Approx. 0.250 kg/ Nos.)'],
+        width: ['Square Cover (Approx. 0.160-0.180 kg) (1 Feet / 1 Side)', 'Square Cover (13 Feet / 16 Feet EPDM Gesket (Approx. 0.016 kg)(1 Feet / 1 Side)'],
+        finish: 'Natural, Anodized, Powder Coated, Wooden Coating, PVDF Coating',
+        RRLChart: [
+          ['Glass Height','24"','30"','36"','42"','48"'],
+          ['Railing Length','&#8734;','&#8734;','20`','10','-']
+        ]
+      } 
+    },
   ];
 
-  architecturalRailingSolutionsImages:any = [
+  continuousSystemImages: any = [
+    { image: 'assets/items/1.png',
+      subImages: [
+        {title:'End Cap',image:'assets/items/1.png'},
+        {title:'Slotted End Cap',image:'assets/items/1.png'}
+      ],
+      category: 'CONTINUOUS SYSTEM',
+      ProductName: 'Commercial feri', 
+      essentialDetails: {
+        type: 'Continuous Profile System',
+        use: 'Indoor and Outdoor',
+        material: 'Aluminium Alloy 6063-T6',
+        length: ['13 Feet / 16 Feet'],
+        width: ['Approx. 0.500kg - 0.530kg / Feet'],
+        finish: 'Natural, Anodized, Powder Coated, Wooden Coating, PVDF Coating',
+        RRLChart: [
+          ['Glass Height','24"','30"','36"','42"','48"'],
+          ['Railing Length','30`','20`','10`','-','-']
+        ]
+      } 
+    }
+  ];
+  
+  bracketSystemImages: any = [
+    { image: 'assets/items/1.png',
+      category: 'BRACKET SYSTEM',
+      ProductName: 'L- Bracket', 
+      essentialDetails: {
+        type: 'Bracket System',
+        use: 'Indoor and Outdoor',
+        material: 'Aluminium Alloy 6063-T6',
+        length: ['Square Bracket (65mm)'],
+        width: ['Approx. 0460 kg / Nos.'],
+        finish: 'Natural, Anodized, Powder Coated, Wooden Coating, PVDF Coating',
+        RRLChart: [
+          ['Glass Height','24"','30"','36"','42"','48"'],
+          ['Railing Length','&#8734;','&#8734;','20`','10','-']
+        ]
+      } 
+    }
+  ];
+  handrailAndAccessoriesImages: any = [
+    { image: 'assets/items/1.png',
+      subImages: [
+        {title:'Wall Concealed',image:'assets/items/1.png'},
+        {title:'Fix Bend',image:'assets/items/1.png'},
+        {title:'Connector',image:'assets/items/1.png'},
+        {title:'End Cap',image:'assets/items/1.png'}
+      ],
+      category: 'HANDRAIL & ACCESSORIES',
+      ProductName: 'RA - HR - 01 (38mm Round)', 
+      essentialDetails: {
+        use: 'Indoor and Outdoor',
+        material: 'Aluminium Alloy 6063-T6',
+        length: ['13 Feet and 16 Feet'],
+        width: ['Approx. 0.170kg - 0.190kg (1 feet)','EPDM Gasket (Approx. 0.056kg/feet)'],
+        finish: 'Natural, Anodized, Powder Coated, Wooden Coating, PVDF Coating',
+      } 
+    }
+  ];
+
+
+  // highlightProductImages: any = [
+  //   { image: 'assets/items/1.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/2.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/3.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/4.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/5.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/6.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/7.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/8.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/9.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/10.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/11.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/12.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/13.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/14.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  //   { image: 'assets/items/15.png', name: 'Glass Railing', desc: 'Elegence tranparency for modern spaces' },
+  // ];
+
+  highlightProductImages:any = [
     { image: 'assets/product/Glass_Railings_1.jpeg', name: 'Glass Railings', desc: 'Elegant transparency for modern spaces' },
     { image: 'assets/product/Aluminium_Railings_1.jpeg', name: 'Aluminium Railings', desc: 'Durable and lightweight solutions' },
     { image: 'assets/product/Cable_Railings_1.jpeg', name: 'Cable Railings', desc: 'Durable and lightweight solutions' },
@@ -46,7 +131,7 @@ export class ProductsComponent {
   ]
 
   exploreProduct: any
-  selectedCategoryName: string = 'Architectural Railing Solutions'
+  selectedCategoryName: string = 'Highlight Products'
   isMobileScreen: boolean = false;
 
   // HostListener to detect window resize events
